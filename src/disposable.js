e02894d7a56c29380c06da6f2cbc293cd3f53b2d
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-20 14:06:33 +0800

    Create disposable.js

---
+++ src/disposable.js
@@ -0,0 +1,21 @@
+/**
+ * @interface
+ * Represents a disposable/cancellable state.
+ */
+// eslint-disable-next-line no-unused-vars
+class Disposable {
+  /**
+   * Disposes the instance
+   * @abstract
+   */
+  // eslint-disable-next-line class-methods-use-this
+  dispose() {}
+
+  /**
+   * Checks if the instance is disposed
+   * @returns {Boolean}
+   * @abstract
+   */
+  // eslint-disable-next-line class-methods-use-this
+  isDisposed() {}
+}
