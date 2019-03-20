11c59ecc486ffe7e4a9d7d8b1826a17af64e3519
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-20 17:24:02 +0800

    Update just.js

--- src/internal/operators/just.js
+++ src/internal/operators/just.js
@@ -2,11 +2,16 @@ import { disposed } from '../utils';
 import Single from '../../single';
 import { error } from '../operators';
 
+/**
+ * @ignore
+ */
 function subscribeActual(observer) {
   observer.onSubscribe(disposed);
   observer.onSuccess(this.value);
 }
-
+/**
+ * @ignore
+ */
 const just = (value) => {
   if (typeof value === 'undefined') {
     return error('Single.just: received an undefined value.');
