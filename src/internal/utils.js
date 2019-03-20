b46619689419f9ae99a390bae642f7db518e1bee
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-20 17:24:08 +0800

    Update utils.js

--- src/internal/utils.js
+++ src/internal/utils.js
@@ -13,7 +13,9 @@ export const disposed = {
   dispose: () => {},
   isDisposed: () => true,
 };
-
+/**
+ * @ignore
+ */
 export const neverDisposed = {
   dispose: () => {},
   isDisposed: () => false,
@@ -22,3 +24,51 @@ export const neverDisposed = {
  * @ignore
  */
 export const toCallable = x => () => x;
+/**
+ * @ignore
+ */
+export const isPromise = obj => (obj instanceof Promise) || (!!obj && (typeof obj === 'object' || typeof obj === 'function') && typeof obj.then === 'function');
+/**
+ * @ignore
+ */
+export function onSuccessHandler(value) {
+  if (this.state === DISPOSED) {
+    return;
+  }
+  const d = this.state;
+  this.state = DISPOSED;
+
+  try {
+    if (typeof value === 'undefined') {
+      this.onError('onSuccess called with undefined.');
+    } else {
+      this.onSuccess(value);
+    }
+  } finally {
+    if (isDisposable(d)) {
+      d.dispose();
+    }
+  }
+}
+/**
+ * @ignore
+ */
+export function onErrorHandler(err) {
+  let report = err;
+  if (typeof err === 'undefined') {
+    report = 'onError called with undefined value.';
+  }
+  if (this.state === DISPOSED) {
+    return;
+  }
+
+  const d = this.state;
+  this.state = DISPOSED;
+  try {
+    this.onError(report);
+  } finally {
+    if (isDisposable(d)) {
+      d.dispose();
+    }
+  }
+}
