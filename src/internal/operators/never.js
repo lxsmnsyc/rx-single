4f910435188b1edefec4ebc700149a07acf76d42
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-20 17:24:06 +0800

    Update never.js

--- src/internal/operators/never.js
+++ src/internal/operators/never.js
@@ -1,10 +1,15 @@
 import { neverDisposed } from '../utils';
 import Single from '../../single';
 
+/**
+ * @ignore
+ */
 function subscribeActual(observer) {
   observer.onSubscribe(neverDisposed);
 }
-
+/**
+ * @ignore
+ */
 const never = () => {
   const single = new Single();
   single.subscribeActual = subscribeActual.bind(single);
