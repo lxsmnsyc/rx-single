fe2fe811c57331b9f1eed3851f21e99447cb2498
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-20 14:11:49 +0800

    Update utils.js

--- src/internal/utils.js
+++ src/internal/utils.js
@@ -1,10 +1,19 @@
+/**
+ * @ignore
+ */
 export const DISPOSED = Symbol('DISPOSED');
-
+/**
+ * @ignore
+ */
 export const isDisposable = obj => typeof obj === 'object' && (typeof obj.dispose === 'function' || typeof obj.isDisposed === 'function');
-
+/**
+ * @ignore
+ */
 export const disposed = {
   dispose: () => {},
   isDisposed: () => true,
 };
-
+/**
+ * @ignore
+ */
 export const toCallable = x => () => x;
