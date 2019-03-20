e49dfc7f92a3267918d7c8e5b16b75da8855d651
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-20 14:06:49 +0800

    Create error.js

---
+++ src/internal/operators/error.js
@@ -0,0 +1,36 @@
+import { disposed, toCallable } from '../utils';
+import Single from '../../single';
+
+function subscribeActual(observer) {
+  observer.onSubscribe(disposed);
+
+  let err;
+
+  try {
+    err = this.supplier();
+
+    if (typeof err === 'undefined') {
+      err = 'Single.error: Error supplier returned an undefined value.';
+    }
+  } catch (e) {
+    err = e;
+  }
+  observer.onError(err);
+}
+
+const error = (value) => {
+  let report = value;
+  if (typeof value === 'undefined') {
+    report = 'Single.error received an undefined value.';
+  }
+
+  if (typeof value !== 'function') {
+    report = toCallable(report);
+  }
+  const single = new Single();
+  single.supplier = report;
+  single.subscribeActual = subscribeActual.bind(single);
+  return single;
+};
+
+export default error;
