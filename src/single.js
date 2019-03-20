d85e4b67bb3ba31fb24f703ca6cb60fa6b416b75
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-20 15:40:08 +0800

    Update single.js

--- src/single.js
+++ src/single.js
@@ -1,8 +1,37 @@
+/**
+ * @license
+ * MIT License
+ *
+ * Copyright (c) 2019 Alexis Munsayac
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *
+ * @author Alexis Munsayac <alexis.munsayac@gmail.com>
+ * @copyright Alexis Munsayac 2019
+ */
 import {
   create, contains, just, error,
 } from './internal/operators';
 
-
+/**
+ *
+ */
 export default class Single {
   /**
    * Creates a Single with a given subscriber.
@@ -10,6 +39,10 @@ export default class Single {
    * This subscriber is a function that receives
    * an object that implements the Emitter interface.
    *
+   * <img src="https://raw.githubusercontent.com/LXSMNSYC/rx-single/master/assets/images/Single.create.png" class="diagram">
+   *
+   * @example
+   * const single = Single.create(e => e.onSuccess('Hello World'));
    * @param {Function} subscriber
    * @returns {Single}
    */
@@ -19,7 +52,8 @@ export default class Single {
 
   /**
    * Creates a Single with an error.
-   * @param {*} err
+   * @param {!(Function|any)} err
+   * @returns {Single}
    */
   static error(err) {
     return error(err);
@@ -27,7 +61,8 @@ export default class Single {
 
   /**
    * Creates a Single with a success value.
-   * @param {*} value
+   * @param {!any} value
+   * @returns {Single}
    */
   static just(value) {
     return just(value);
@@ -46,6 +81,8 @@ export default class Single {
    * or subscribe is executed. This method receives an
    * object that implements the Disposable interface.
    *
+   * @param {!Object} observer
+   * @returns {undefined}
    */
   subscribeWith(observer) {
     this.subscribeActual(observer);
@@ -60,8 +97,8 @@ export default class Single {
    * onError receives a string(or an Error object).
    *
    * Both are called once.
-   * @param {Function} onSuccess
-   * @param {Function} onError
+   * @param {?Function} onSuccess
+   * @param {?Function} onError
    * @returns {Disposable} returns an object that implements the
    *  Disposable interface.
    */
@@ -91,7 +128,7 @@ export default class Single {
   /**
    * Compares the success value of the Single to a given
    * value, and emits the boolean result.
-   * @param {*} value
+   * @param {any} value
    * @param {?Function} comparer
    * @returns {Single}
    */
