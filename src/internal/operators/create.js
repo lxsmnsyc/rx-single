067116272ed32cb53dc641209b4467be52f7f535
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-20 14:06:44 +0800

    Create create.js

---
+++ src/internal/operators/create.js
@@ -0,0 +1,78 @@
+import { DISPOSED, isDisposable } from '../utils';
+import Single from '../../single';
+
+function subscribeActual(observer) {
+  let disposable;
+  const emitter = {
+    onSuccess(value) {
+      if (disposable === DISPOSED) {
+        return;
+      }
+      const d = disposable;
+      disposable = DISPOSED;
+
+      try {
+        if (typeof value === 'undefined') {
+          observer.onError('onSuccess called with undefined.');
+        } else {
+          observer.onSuccess(value);
+        }
+      } finally {
+        if (isDisposable(d)) {
+          d.dispose();
+        }
+      }
+    },
+    onError(err) {
+      let report = err;
+      if (typeof err === 'undefined') {
+        report = 'onError called with undefined value.';
+      }
+      if (disposable === DISPOSED) {
+        return;
+      }
+
+      const d = disposable;
+      disposable = DISPOSED;
+      try {
+        observer.onError(report);
+      } finally {
+        if (isDisposable(d)) {
+          d.dispose();
+        }
+      }
+    },
+    setDisposable(d) {
+      if (isDisposable(d)) {
+        disposable = d;
+      }
+    },
+    dispose() {
+      if (isDisposable(disposable)) {
+        disposable.dispose();
+      }
+      disposable = DISPOSED;
+    },
+    isDisposed() {
+      return disposable === DISPOSED;
+    },
+  };
+  observer.onSubscribe(emitter);
+  try {
+    this.subscriber(emitter);
+  } catch (ex) {
+    emitter.onError(ex);
+  }
+}
+
+const create = (subscriber) => {
+  const single = new Single();
+  single.subscriber = subscriber;
+  /**
+   * @ignore
+   */
+  single.subscribeActual = subscribeActual.bind(single);
+  return single;
+};
+
+export default create;
