8d03707c052820501c7e8dbc86d20820965c1eb5
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-20 16:05:04 +0800

    Update operators.js

--- src/internal/operators.js
+++ src/internal/operators.js
@@ -5,5 +5,6 @@
 export { default as create } from './operators/create';
 export { default as contains } from './operators/contains';
 export { default as defer } from './operators/defer';
+export { default as delay } from './operators/delay';
 export { default as error } from './operators/error';
 export { default as just } from './operators/just';
