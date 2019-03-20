11d1ac1511fd14b1f28e1952225c5df839a40008
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-20 14:06:35 +0800

    Create emitter.js

---
+++ src/emitter.js
@@ -0,0 +1,46 @@
+/**
+ * @interface
+ * Represents an object that receives notification to
+ * an Observer.
+ *
+ * Emitter is an abstraction layer of the Observer
+ */
+// eslint-disable-next-line no-unused-vars
+class Emitter {
+  /**
+   * Emits a success value.
+   * @abstract
+   */
+  // eslint-disable-next-line class-methods-use-this, no-unused-vars
+  onSuccess(value) {}
+
+  /**
+   * Emits an error value.
+   * @abstract
+   */
+  // eslint-disable-next-line class-methods-use-this, no-unused-vars
+  onError(err) {}
+
+  /**
+   * Sets the Disposable for this instance
+   * @param {Disposable} d
+   * @abstract
+   */
+  // eslint-disable-next-line class-methods-use-this, no-unused-vars
+  setDisposable(d) {}
+
+  /**
+   * Disposes the instance
+   * @abstract
+   */
+  // eslint-disable-next-line class-methods-use-this
+  dispose() {}
+
+  /**
+   * Checks if the instance is disposed
+   * @returns {Boolean}
+   * @abstract
+   */
+  // eslint-disable-next-line class-methods-use-this
+  isDisposed() {}
+}
