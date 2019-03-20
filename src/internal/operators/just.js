dc62fc5db8ebf83c77c87bf589c95d428785ae66
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-20 14:06:53 +0800

    Create just.js

---
+++ src/internal/operators/just.js
@@ -0,0 +1,21 @@
+import { disposed } from '../utils';
+import Single from '../../single';
+import { error } from '../operators';
+
+
+function subscribeActual(observer) {
+  observer.onSubscribe(disposed);
+  observer.onSuccess(this.value);
+}
+
+const just = (value) => {
+  if (typeof value === 'undefined') {
+    return error('Single.just: received an undefined value.');
+  }
+  const single = new Single();
+  single.value = value;
+  single.subscribeActual = subscribeActual.bind(single);
+  return single;
+};
+
+export default just;
