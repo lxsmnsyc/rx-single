36bb7737d2d9d12795ae39f0bfd3a25d3bd258cb
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-20 14:06:59 +0800

    Create operators.js

---
+++ src/internal/operators.js
@@ -0,0 +1,5 @@
+/* eslint-disable import/no-cycle */
+export { default as create } from './operators/create';
+export { default as contains } from './operators/contains';
+export { default as error } from './operators/error';
+export { default as just } from './operators/just';
