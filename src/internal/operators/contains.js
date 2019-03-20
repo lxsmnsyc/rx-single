4f5e8adc15e6e0c53399732bedc32a7cc0df5272
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-20 14:06:46 +0800

    Create contains.js

---
+++ src/internal/operators/contains.js
@@ -0,0 +1,43 @@
+import Single from '../../single';
+
+const containsComparer = (x, y) => x === y;
+
+function subscribeActual(observer) {
+  const { onSuccess, onError, onSubscribe } = observer;
+
+  const { value, comparer } = this;
+
+  this.source.subscribeWith({
+    onSubscribe,
+    onSuccess(x) {
+      let result;
+      try {
+        result = comparer(x, value);
+      } catch (e) {
+        onError(e);
+      }
+      onSuccess(result);
+    },
+    onError,
+  });
+}
+
+const contains = (source, value, comparer) => {
+  if (typeof value === 'undefined') {
+    return source;
+  }
+
+  let cmp = comparer;
+  if (typeof cmp !== 'function') {
+    cmp = containsComparer;
+  }
+
+  const single = new Single();
+  single.source = source;
+  single.value = value;
+  single.comparer = cmp;
+  single.subscribeActual = subscribeActual.bind(single);
+  return single;
+};
+
+export default contains;
