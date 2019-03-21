7a0f4d172ba82a3023043e4ca7a9edf4e1ed6086
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-21 16:18:28 +0800

    Create onErrorResumeNext.js

---
+++ src/internal/operators/onErrorResumeNext.js
@@ -0,0 +1,57 @@
+import Single from '../../single';
+import { SimpleDisposable } from '../utils';
+
+function subscribeActual(observer) {
+  const { onSuccess, onError, onSubscribe } = observer;
+
+  const { source, resumeIfError } = this;
+
+  const disposable = new SimpleDisposable();
+
+  source.subscribeWith({
+    onSubscribe(d) {
+      disposable.setDisposable(d);
+      onSubscribe(disposable);
+    },
+    onSuccess,
+    onError(x) {
+      let result;
+
+      if (typeof resumeIfError === 'function') {
+        try {
+          result = resumeIfError(x);
+          if (typeof result === 'undefined') {
+            throw new Error('Single.onErrorResumeNext: returned an non-Single.');
+          }
+        } catch (e) {
+          onError([x, e]);
+          return;
+        }
+      } else {
+        result = resumeIfError;
+      }
+
+      result.subscribeWith({
+        onSubscribe(d) {
+          disposable.setDisposable(d);
+        },
+        onSuccess,
+        onError,
+      });
+    },
+  });
+}
+
+const onErrorResumeNext = (source, resumeIfError) => {
+  if (!(typeof resumeIfError === 'function' || resumeIfError instanceof Single)) {
+    return source;
+  }
+
+  const single = new Single();
+  single.source = source;
+  single.resumeIfError = resumeIfError;
+  single.subscribeActual = subscribeActual.bind(single);
+  return single;
+};
+
+export default onErrorResumeNext;
