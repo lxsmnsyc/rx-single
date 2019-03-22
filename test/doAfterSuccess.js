d715fd92c6114d6700050568d16230f5b975f678
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-22 14:55:34 +0800

    Create doAfterSuccess.js

---
+++ test/doAfterSuccess.js
@@ -0,0 +1,41 @@
+/* eslint-disable no-undef */
+import assert from 'assert';
+import Single from '../src/single';
+
+/**
+ *
+ */
+describe('Single', () => {
+  /**
+   *
+   */
+  describe('#doAfterSuccess', () => {
+    /**
+     *
+     */
+    it('should create a Single', () => {
+      const single = Single.just('Hello').doAfterSuccess(x => console.log(`after success: ${x}`));
+      assert(single instanceof Single);
+    });
+    /**
+     *
+     */
+    it('should return the same instance if the method received a non-function parameter.', () => {
+      const source = Single.just('Hello');
+      const single = source.doAfterSuccess();
+      assert(source === single);
+    });
+    /**
+     *
+     */
+    it('should call the given function after success.', (done) => {
+      let called;
+      const source = Single.just('Hello');
+      const single = source.doAfterSuccess(() => called && done());
+      single.subscribe(
+        () => { called = true; },
+        () => done(false),
+      );
+    });
+  });
+});
