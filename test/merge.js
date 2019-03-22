1dd4c0f85418ec12f349af72b93c3ebeaea1b609
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-22 17:00:10 +0800

    Create merge.js

---
+++ test/merge.js
@@ -0,0 +1,55 @@
+/* eslint-disable no-undef */
+import assert from 'assert';
+import Single from '../src/single';
+
+/**
+ *
+ */
+describe('Single', () => {
+  /**
+   *
+   */
+  describe('#merge', () => {
+    /**
+     *
+     */
+    it('should create a Single', () => {
+      const single = Single.merge(Single.just(Single.just('Hello')));
+
+      assert(single instanceof Single);
+    });
+    /**
+     *
+     */
+    it('should signal error if no source is provided', (done) => {
+      const single = Single.merge();
+
+      single.subscribe(
+        () => done(false),
+        () => done(),
+      );
+    });
+    /**
+     *
+     */
+    it('should signal error if source emitted a non-Single', (done) => {
+      const single = Single.merge(Single.just('Hello'));
+
+      single.subscribe(
+        () => done(false),
+        () => done(),
+      );
+    });
+    /**
+     *
+     */
+    it('should signal success of the signaled Single', (done) => {
+      const single = Single.merge(Single.just(Single.just('Hello')));
+
+      single.subscribe(
+        x => (x === 'Hello' ? done() : done(false)),
+        () => done(false),
+      );
+    });
+  });
+});
