39ce4727940c3f4577959c10d53d36ad5c19924f
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-22 14:55:38 +0800

    Create doFinally.js

---
+++ test/doFinally.js
@@ -0,0 +1,66 @@
+/* eslint-disable no-undef */
+import assert from 'assert';
+import Single from '../src/single';
+
+/**
+ *
+ */
+describe('Single', () => {
+  /**
+   *
+   */
+  describe('#doFinally', () => {
+    /**
+     *
+     */
+    it('should create a Single', () => {
+      const single = Single.just('Hello').doFinally(() => {});
+      assert(single instanceof Single);
+    });
+    /**
+     *
+     */
+    it('should return the same instance if the method received a non-function parameter.', () => {
+      const source = Single.just('Hello');
+      const single = source.doFinally();
+      assert(source === single);
+    });
+    /**
+     *
+     */
+    it('should call the given function after success.', (done) => {
+      let called;
+      const source = Single.just('Hello');
+      const single = source.doFinally(() => called && done());
+      single.subscribe(
+        () => { called = true; },
+        () => done(false),
+      );
+    });
+    /**
+     *
+     */
+    it('should call the given function after error.', (done) => {
+      let called;
+      const source = Single.error('Hello');
+      const single = source.doFinally(() => called && done());
+      single.subscribe(
+        () => done(false),
+        () => { called = true; },
+      );
+    });
+    /**
+     *
+     */
+    it('should call the given function on dispose.', (done) => {
+      const source = Single.timer(100);
+      const single = source.doFinally(() => done());
+
+      const disposable = single.subscribe(
+        () => done(false),
+        () => done(false),
+      );
+      disposable.dispose();
+    });
+  });
+});
