2e21f785654e718ce02c33e7c8eae23d34e5136a
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-22 16:59:56 +0800

    Create merge.js

---
+++ src/internal/operators/merge.js
@@ -0,0 +1,44 @@
+import Single from '../../single';
+import error from './error';
+import { SimpleDisposable } from '../utils';
+
+function subscribeActual(observer) {
+  const { onSubscribe, onError, onSuccess } = observer;
+
+  const disposable = new SimpleDisposable();
+
+  onSubscribe(disposable);
+
+  this.source.subscribeWith({
+    onSubscribe(d) {
+      disposable.setDisposable(d);
+    },
+    onSuccess(x) {
+      let result = x;
+      if (!(x instanceof Single)) {
+        result = error('Single.merge: source emitted a non-Single value.');
+      }
+      result.subscribeWith({
+        onSubscribe(d) {
+          disposable.setDisposable(d);
+        },
+        onSuccess,
+        onError,
+      });
+    },
+    onError,
+  });
+}
+
+const merge = (source) => {
+  if (!(source instanceof Single)) {
+    return error('Single.merge: source is not a Single.');
+  }
+
+  const single = new Single();
+  single.source = source;
+  single.subscribeActual = subscribeActual.bind(single);
+  return single;
+};
+
+export default merge;
