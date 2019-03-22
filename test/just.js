e582bd115df5c53b66e523e59800e8a1ff087e33
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-22 14:56:06 +0800

    Create just.js

---
+++ test/just.js
@@ -0,0 +1,44 @@
+/* eslint-disable no-undef */
+import assert from 'assert';
+import Single from '../src/single';
+
+/**
+ *
+ */
+describe('Single', () => {
+  /**
+   *
+   */
+  describe('#just', () => {
+    /**
+     *
+     */
+    it('should create a Single', () => {
+      const single = Single.just('Hello World');
+
+      assert(single instanceof Single);
+    });
+    /**
+     *
+     */
+    it('should succeed with the given value.', (done) => {
+      const single = Single.just('Hello World');
+
+      single.subscribe(
+        x => (x === 'Hello World' ? done() : done(false)),
+        e => done(e),
+      );
+    });
+    /**
+     *
+     */
+    it('should emit error if value is undefined.', (done) => {
+      const single = Single.just();
+
+      single.subscribe(
+        () => done(false),
+        e => (typeof e !== 'undefined' ? done() : done(false)),
+      );
+    });
+  });
+});
