83f683b17703aaed9134287de530355ba1c4d9ff
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-22 15:50:10 +0800

    Update doOnDispose.js

--- test/doOnDispose.js
+++ test/doOnDispose.js
@@ -28,6 +28,34 @@ describe('Single', () => {
     /**
      *
      */
+    it('should signal the success value then fire dispose callback.', (done) => {
+      let called;
+      const source = Single.just('Hello');
+      const single = source.doOnDispose(() => called && done());
+
+      const disposable = single.subscribe(
+        () => { called = true; },
+        done,
+      );
+      disposable.dispose();
+    });
+    /**
+     *
+     */
+    it('should signal the error value then fire dispose callback.', (done) => {
+      let called;
+      const source = Single.error('Hello');
+      const single = source.doOnDispose(() => called && done());
+
+      const disposable = single.subscribe(
+        done,
+        () => { called = true; },
+      );
+      disposable.dispose();
+    });
+    /**
+     *
+     */
     it('should call the given function on dispose.', (done) => {
       const source = Single.just('Hello').delay(100);
       const single = source.doOnDispose(() => done());
