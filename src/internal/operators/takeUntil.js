d3b7ec5656907640b832302c1c23c2d3383be54b
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-22 18:19:39 +0800

    Create takeUntil.js

---
+++ src/internal/operators/takeUntil.js
@@ -0,0 +1,56 @@
+import Single from '../../single';
+import { CompositeDisposable } from '../utils';
+
+function subscribeActual(observer) {
+  const { onSubscribe, onSuccess, onError } = observer;
+
+  const disposable = new CompositeDisposable();
+
+  onSubscribe(disposable);
+
+  const { source, other } = this;
+
+  if (!disposable.isDisposed()) {
+    other.subscribeWith({
+      onSubscribe(d) {
+        disposable.add(d);
+      },
+      onSuccess() {
+        onError('Single.takeUntil: Source cancelled by other Single.');
+        disposable.dispose();
+      },
+      onError(x) {
+        onError(['Single.takeUntil: Source cancelled by other Single.', x]);
+        disposable.dispose();
+      },
+    });
+
+    source.subscribeWith({
+      onSubscribe(d) {
+        disposable.add(d);
+      },
+      onSuccess(x) {
+        onSuccess(x);
+        disposable.dispose();
+      },
+      onError(x) {
+        onError(x);
+        disposable.dispose();
+      },
+    });
+  }
+}
+
+const takeUntil = (source, other) => {
+  if (!(other instanceof Single)) {
+    return source;
+  }
+
+  const single = new Single();
+  single.source = source;
+  single.other = other;
+  single.subscribeActual = subscribeActual.bind(single);
+  return single;
+};
+
+export default takeUntil;
