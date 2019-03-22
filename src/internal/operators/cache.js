7427ea4d45b9155407afbc2043234d50806aab94
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-22 13:18:01 +0800

    Create cache.js

---
+++ src/internal/operators/cache.js
@@ -0,0 +1,71 @@
+import Single from '../../single';
+import { disposed, SimpleDisposable } from '../utils';
+
+
+function subscribeActual(observer) {
+  const { onSuccess, onError, onSubscribe } = observer;
+
+  const {
+    source, cached, observers, subscribed,
+  } = this;
+
+  if (!cached) {
+    const index = observers.length;
+    observers[index] = observer;
+
+    onSubscribe(new SimpleDisposable(() => {
+      observers.splice(index, 1);
+    }));
+
+    if (!subscribed) {
+      source.subscribeWith({
+        onSubscribe() {
+          // not applicable
+        },
+        onSuccess: (x) => {
+          this.cached = true;
+          this.value = x;
+
+          // eslint-disable-next-line no-restricted-syntax
+          for (const obs of observers) {
+            obs.onSuccess(x);
+          }
+          this.observers = undefined;
+        },
+        onError: (x) => {
+          this.cached = true;
+          this.error = x;
+
+          // eslint-disable-next-line no-restricted-syntax
+          for (const obs of observers) {
+            obs.onError(x);
+          }
+          this.observers = undefined;
+        },
+      });
+      this.subscribed = true;
+    }
+  } else {
+    onSubscribe(disposed);
+
+    const { value, error } = this;
+    if (typeof value !== 'undefined') {
+      onSuccess(value);
+    }
+    if (typeof error !== 'undefined') {
+      onError(value);
+    }
+  }
+}
+
+const cache = (source) => {
+  const single = new Single();
+  single.source = source;
+  single.cached = false;
+  single.subscribed = false;
+  single.observers = [];
+  single.subscribeActual = subscribeActual.bind(single);
+  return single;
+};
+
+export default cache;
