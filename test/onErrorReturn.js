3ed6b15763937a8a69c36d11f872fed66e13c251
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-22 14:56:18 +0800

    Create onErrorReturn.js

---
+++ test/onErrorReturn.js
@@ -0,0 +1,60 @@
+/* eslint-disable no-undef */
+import assert from 'assert';
+import Single from '../src/single';
+
+/**
+ *
+ */
+describe('Single', () => {
+  /**
+   *
+   */
+  describe('#onErrorReturn', () => {
+    /**
+     *
+     */
+    it('should create a Single', () => {
+      const single = Single.error('Hello').onErrorReturn(() => 'World');
+      assert(single instanceof Single);
+    });
+    /**
+     *
+     */
+    it('should return the same instance if parameter passed is not a Single or a function', () => {
+      const source = Single.error('Hello');
+      const single = source.onErrorReturn();
+      assert(single === source);
+    });
+    /**
+     *
+     */
+    it('should emit the supplied item by the given function in case of error', (done) => {
+      const single = Single.error('Hello').onErrorReturn(() => 'World');
+
+      single.subscribe(
+        x => (x === 'World' ? done() : done(false)),
+        done,
+      );
+    });
+    /**
+     *
+     */
+    it('should emit error if provide function throws error.', (done) => {
+      const single = Single.error('Hello').onErrorReturn(() => { throw new Error('Ooops'); });
+      single.subscribe(
+        done,
+        () => done(),
+      );
+    });
+    /**
+     *
+     */
+    it('should emit error if provide function returns undefined.', (done) => {
+      const single = Single.error('Hello').onErrorReturn(() => {});
+      single.subscribe(
+        done,
+        () => done(),
+      );
+    });
+  });
+});
