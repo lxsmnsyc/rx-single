8e7a1eca397a110c4984f090686fcdde93c720dc
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-22 17:09:21 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+/* eslint-disable no-unused-vars */
 import Single from './single';
+import Disposable from './disposable';
+import Emitter from './emitter';
+import Observer from './observer';
 
 export default Single;
