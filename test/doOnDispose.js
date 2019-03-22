c75128bd7c41a445d5f28002694a9d8f9f490cfb
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-22 14:55:42 +0800

    Create doOnDispose.js

---
+++ test/doOnDispose.js
@@ -0,0 +1,42 @@
+/* eslint-disable no-undef */
+import assert from 'assert';
+import Single from '../src/single';
+
+/**
+ *
+ */
+describe('Single', () => {
+  /**
+   *
+   */
+  describe('#doOnDispose', () => {
+    /**
+     *
+     */
+    it('should create a Single', () => {
+      const single = Single.just('Hello').doOnDispose(() => {});
+      assert(single instanceof Single);
+    });
+    /**
+     *
+     */
+    it('should return the same instance if the method received a non-function parameter.', () => {
+      const source = Single.just('Hello');
+      const single = source.doOnDispose();
+      assert(source === single);
+    });
+    /**
+     *
+     */
+    it('should call the given function on dispose.', (done) => {
+      const source = Single.just('Hello');
+      const single = source.doOnDispose(() => done());
+
+      const disposable = single.subscribe(
+        () => done(false),
+        () => done(false),
+      );
+      disposable.dispose();
+    });
+  });
+});
