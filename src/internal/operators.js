6efa6c56f2a0b0d81630aa5ac36f57348a0689de
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-22 11:35:42 +0800

    Update operators.js

--- src/internal/operators.js
+++ src/internal/operators.js
@@ -16,6 +16,7 @@ export { default as doOnError } from './operators/doOnError';
 export { default as doOnEvent } from './operators/doOnEvent';
 export { default as doOnSuccess } from './operators/doOnSuccess';
 export { default as doOnSubscribe } from './operators/doOnSubscribe';
+export { default as doOnTerminate } from './operators/doOnTerminate';
 export { default as error } from './operators/error';
 export { default as fromCallable } from './operators/fromCallable';
 export { default as fromPromise } from './operators/fromPromise';
