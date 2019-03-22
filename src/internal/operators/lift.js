e87d817b796393d0a17397e02e948dcaf3b19ab5
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-22 17:54:07 +0800

    Create lift.js

---
+++ src/internal/operators/lift.js
@@ -0,0 +1,33 @@
+import Single from '../../single';
+import { isObserver, immediateError } from '../utils';
+
+function subscribeActual(observer) {
+  let result;
+
+  try {
+    result = this.operator(observer);
+
+    if (!isObserver(result)) {
+      throw new Error('Single.lift: operator returned a non-Observer.');
+    }
+  } catch (e) {
+    immediateError(observer, e);
+    return;
+  }
+
+  this.source.subscribeWith(result);
+}
+
+const lift = (source, operator) => {
+  if (typeof operator !== 'function') {
+    return source;
+  }
+
+  const single = new Single();
+  single.source = source;
+  single.operator = operator;
+  single.subscribeActual = subscribeActual.bind(single);
+  return single;
+};
+
+export default lift;
