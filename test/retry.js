b58ab8ddc50e2e2dbe4914740cf331e6db3c0ede
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-22 17:00:12 +0800

    Create retry.js

---
+++ test/retry.js
@@ -0,0 +1,62 @@
+/* eslint-disable no-undef */
+import assert from 'assert';
+import Single from '../src/single';
+import { doOnError } from '../src/internal/operators';
+
+/**
+ *
+ */
+describe('Single', () => {
+  /**
+   *
+   */
+  describe('#retry', () => {
+    /**
+     *
+     */
+    it('should create a Single', () => {
+      const single = Single.error('Hello').retry(x => x === 3);
+
+      assert(single instanceof Single);
+    });
+    /**
+     *
+     */
+    it('should signal a success if no error', (done) => {
+      const single = Single.just('Hello').retry(x => x === 3);
+
+      single.subscribe(
+        () => done(),
+        () => done(false),
+      );
+    });
+    /**
+     *
+     */
+    it('should retry if there is an error and if it passes the predicate', (done) => {
+      let retried;
+      const single = Single.error('Hello').retry((x) => {
+        if (x === 2) {
+          retried = true;
+        }
+        return x < 3;
+      });
+
+      single.subscribe(
+        () => done(false),
+        () => retried && done(),
+      );
+    });
+    /**
+     *
+     */
+    it('should signal an error if predicate is false', (done) => {
+      const single = Single.error('Hello').retry(x => x === 3);
+
+      single.subscribe(
+        () => done(false),
+        () => done(),
+      );
+    });
+  });
+});
