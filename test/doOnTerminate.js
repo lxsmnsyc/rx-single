b2209981795bd1cc85927b5b89aeab2dde6585c7
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-22 14:55:54 +0800

    Create doOnTerminate.js

---
+++ test/doOnTerminate.js
@@ -0,0 +1,53 @@
+/* eslint-disable no-undef */
+import assert from 'assert';
+import Single from '../src/single';
+
+/**
+ *
+ */
+describe('Single', () => {
+  /**
+   *
+   */
+  describe('#doOnTerminate', () => {
+    /**
+     *
+     */
+    it('should create a Single', () => {
+      const single = Single.just('Hello').doOnTerminate(() => {});
+      assert(single instanceof Single);
+    });
+    /**
+     *
+     */
+    it('should return the same instance if the method received a non-function parameter.', () => {
+      const source = Single.just('Hello');
+      const single = source.doOnTerminate();
+      assert(source === single);
+    });
+    /**
+     *
+     */
+    it('should call the given function on success.', (done) => {
+      let called;
+      const source = Single.just('Hello');
+      const single = source.doOnTerminate(() => { called = true; });
+      single.subscribe(
+        () => called && done(),
+        () => done(false),
+      );
+    });
+    /**
+     *
+     */
+    it('should call the given function on error.', (done) => {
+      let called;
+      const source = Single.error('Hello');
+      const single = source.doOnTerminate(() => { called = true; });
+      single.subscribe(
+        () => done(false),
+        () => called && done(),
+      );
+    });
+  });
+});
