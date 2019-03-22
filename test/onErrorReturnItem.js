e6b41c6eaaab070e1ffd74945537adcb672d263e
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-22 14:56:20 +0800

    Create onErrorReturnItem.js

---
+++ test/onErrorReturnItem.js
@@ -0,0 +1,41 @@
+/* eslint-disable no-undef */
+import assert from 'assert';
+import Single from '../src/single';
+
+/**
+ *
+ */
+describe('Single', () => {
+
+  /**
+   *
+   */
+  describe('#onErrorReturnItem', () => {
+    /**
+     *
+     */
+    it('should create a Single', () => {
+      const single = Single.error('Hello').onErrorReturnItem('World');
+      assert(single instanceof Single);
+    });
+    /**
+     *
+     */
+    it('should return the same instance if parameter passed is not a Single or a function', () => {
+      const source = Single.error('Hello');
+      const single = source.onErrorReturnItem();
+      assert(single === source);
+    });
+    /**
+     *
+     */
+    it('should emit the given item in case of error', (done) => {
+      const single = Single.error('Hello').onErrorReturnItem('World');
+
+      single.subscribe(
+        x => (x === 'World' ? done() : done(false)),
+        done,
+      );
+    });
+  });
+});
