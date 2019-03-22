6b7a6a7cc5de8d971cebfc5ceaac768a6d4ad534
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-22 14:56:23 +0800

    Create then.js

---
+++ test/then.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-undef */
+import assert from 'assert';
+import Single from '../src/single';
+
+/**
+ *
+ */
+describe('Single', () => {
+  /**
+   *
+   */
+  describe('#then', () => {
+    /**
+     *
+     */
+    it('should create a Promise', () => {
+      const single = Single.just('Hello').then(x => x, x => x);
+      assert(single instanceof Promise);
+    });
+  });
+});
