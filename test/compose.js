4c213ff2d7cba031c08127c311d37518458ddbdf
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-22 17:33:22 +0800

    Create compose.js

---
+++ test/compose.js
@@ -0,0 +1,53 @@
+/* eslint-disable no-undef */
+import assert from 'assert';
+import Single from '../src/single';
+
+/**
+ *
+ */
+describe('Single', () => {
+  /**
+   *
+   */
+  describe('#compose', () => {
+    /**
+     *
+     */
+    it('should return the same instance if no function is provided', () => {
+      const source = Single.just('Hello');
+      const single = source.compose();
+
+      assert(source === single);
+    });
+    /**
+     *
+     */
+    it('should return a Single from the transformer', () => {
+      const single = Single.just('Hello').compose(source => source.map(x => `${x} World`));
+
+      assert(single instanceof Single);
+    });
+    /**
+     *
+     */
+    it('should correctly signal the composed Single', (done) => {
+      const single = Single.just('Hello').compose(source => source.map(x => `${x} World`));
+
+      single.subscribe(
+        x => (x === 'Hello World' ? done() : done(false)),
+        done,
+      );
+    });
+    /**
+     *
+     */
+    it('should signal error if the transformer function returned a non-Single', (done) => {
+      const single = Single.just('Hello').compose(() => undefined);
+
+      single.subscribe(
+        done,
+        () => done(),
+      );
+    });
+  });
+});
