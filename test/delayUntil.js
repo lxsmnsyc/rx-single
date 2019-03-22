736a569247b173cac0d5715b58dad20580d1b8a5
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-22 14:55:32 +0800

    Create delayUntil.js

---
+++ test/delayUntil.js
@@ -0,0 +1,79 @@
+/* eslint-disable no-undef */
+import assert from 'assert';
+import Single from '../src/single';
+
+/**
+ *
+ */
+describe('Single', () => {
+  /**
+   *
+   */
+  describe('#delayUntil', () => {
+    /**
+     *
+     */
+    it('should create a Single', () => {
+      const single = Single.just('Hello').delayUntil(Single.timer(100));
+      assert(single instanceof Single);
+    });
+    /**
+     *
+     */
+    it('should return the same instance if the provided value is not a Single.', () => {
+      const source = Single.just('Hello');
+      const single = source.delayUntil();
+      assert(source === single);
+    });
+    /**
+     *
+     */
+    it('should signal success with the given value.', (done) => {
+      const single = Single.just('Hello').delayUntil(Single.timer(100));
+      single.subscribe(
+        x => (x === 'Hello' ? done() : done(false)),
+        x => done(x),
+      );
+    });
+    /**
+     *
+     */
+    it('should signal error with the given value.', (done) => {
+      const single = Single.error('Hello').delayUntil(Single.timer(100));
+      single.subscribe(
+        x => done(x),
+        x => (x === 'Hello' ? done() : done(false)),
+      );
+    });
+    /**
+     *
+     */
+    it('should not signal success if disposed.', (done) => {
+      const source = Single.just('Hello').delayUntil(Single.timer(100));
+      const disposable = source.subscribe(
+        () => done(false),
+        () => done(false),
+      );
+
+      disposable.dispose();
+      if (disposable.isDisposed()) {
+        done();
+      }
+    });
+    /**
+     *
+     */
+    it('should not signal error if disposed.', (done) => {
+      const source = Single.error('Hello').delayUntil(Single.timer(100));
+      const disposable = source.subscribe(
+        () => done(false),
+        () => done(false),
+      );
+
+      disposable.dispose();
+      if (disposable.isDisposed()) {
+        done();
+      }
+    });
+  });
+});
