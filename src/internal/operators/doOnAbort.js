e4e753598f7c5dc1812c4177ff00837b32c48f8a
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-29 11:16:33 +0800

    Create doOnAbort.js

---
+++ src/internal/operators/doOnAbort.js
@@ -0,0 +1,34 @@
+import Single from '../../single';
+
+/**
+ * @ignore
+ */
+function subscribeActual(observer) {
+  const { onSubscribe, onSuccess, onError } = observer;
+
+  const { source, callable } = this;
+
+  source.subscribeWith({
+    onSubscribe(ac) {
+      ac.signal.addEventListener('abort', callable);
+      onSubscribe(ac);
+    },
+    onSuccess,
+    onError,
+  });
+}
+
+/**
+ * @ignore
+ */
+export default (source, callable) => {
+  if (typeof callable !== 'function') {
+    return source;
+  }
+
+  const single = new Single();
+  single.source = source;
+  single.callable = callable;
+  single.subscribeActual = subscribeActual.bind(single);
+  return single;
+};
