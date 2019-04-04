8177d4edd48b82abdadc0ed7ac1e12bdd274fc92
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-04 14:39:32 +0800

    Update doOnEvent.js

--- src/internal/operators/doOnEvent.js
+++ src/internal/operators/doOnEvent.js
@@ -1,5 +1,5 @@
 import Single from '../../single';
-import { cleanObserver } from '../utils';
+import { cleanObserver, isFunction } from '../utils';
 
 /**
  * @ignore
@@ -26,7 +26,7 @@ function subscribeActual(observer) {
  * @ignore
  */
 export default (source, callable) => {
-  if (typeof callable !== 'function') {
+  if (!isFunction(callable)) {
     return source;
   }
 
