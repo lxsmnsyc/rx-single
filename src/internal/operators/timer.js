def1be3b81c75afb6e22d8035bea07bc87e3c2c8
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-10 09:45:19 +0800

    Update timer.js

--- src/internal/operators/timer.js
+++ src/internal/operators/timer.js
@@ -1,14 +1,14 @@
 import AbortController from 'abort-controller';
+import Scheduler from 'rx-scheduler';
 import Single from '../../single';
 import { cleanObserver, isNumber } from '../utils';
-import { error } from '../operators';
+import error from './error';
 
 /**
  * @ignore
  */
 function subscribeActual(observer) {
   const { onSuccess, onSubscribe } = cleanObserver(observer);
-
 
   const controller = new AbortController();
 
@@ -20,20 +20,24 @@ function subscribeActual(observer) {
     return;
   }
 
-  const timeout = setTimeout(onSuccess, this.amount, 0);
+  const timeout = this.scheduler.delay(() => onSuccess(0), this.amount);
 
-  signal.addEventListener('abort', () => {
-    clearTimeout(timeout);
-  });
+  signal.addEventListener('abort', () => timeout.abort());
 }
 /**
  * @ignore
  */
-export default (amount) => {
+export default (amount, scheduler) => {
   if (!isNumber(amount)) {
     return error(new Error('Single.timer: "amount" is not a number.'));
   }
+
+  let sched = scheduler;
+  if (!(sched instanceof Scheduler.interface)) {
+    sched = Scheduler.current;
+  }
   const single = new Single(subscribeActual);
   single.amount = amount;
+  single.scheduler = sched;
   return single;
 };
