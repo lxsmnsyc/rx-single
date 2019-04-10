6c9621a31e73380355ad2c4f9db4288c85579a56
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-10 09:45:23 +0800

    Create subscribeOn.js

---
+++ src/internal/operators/subscribeOn.js
@@ -0,0 +1,51 @@
+import AbortController from 'abort-controller';
+import Scheduler from 'rx-scheduler';
+import Single from '../../single';
+import { cleanObserver } from '../utils';
+
+function subscribeActual(observer) {
+  const { onSubscribe, onSuccess, onError } = cleanObserver(observer);
+
+  const { source, scheduler } = this;
+
+  const controller = new AbortController();
+  onSubscribe(controller);
+
+  const { signal } = controller;
+
+  if (signal.aborted) {
+    return;
+  }
+
+  scheduler.schedule(() => {
+    if (signal.aborted) {
+      return;
+    }
+    source.subscribeWith({
+      onSubscribe(ac) {
+        signal.addEventListener('abort', () => ac.abort());
+      },
+      onSuccess(x) {
+        onSuccess(x);
+        controller.abort();
+      },
+      onError(x) {
+        onError(x);
+        controller.abort();
+      },
+    });
+  });
+}
+/**
+ * @ignore
+ */
+export default (source, scheduler) => {
+  let sched = scheduler;
+  if (!(sched instanceof Scheduler.interface)) {
+    sched = Scheduler.current;
+  }
+  const single = new Single(subscribeActual);
+  single.source = source;
+  single.scheduler = sched;
+  return single;
+};
