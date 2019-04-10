48a3cb9e90344c0073249b8a7e5dbafbf79e4220
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-10 09:45:31 +0800

    Update delay.js

--- src/internal/operators/delay.js
+++ src/internal/operators/delay.js
@@ -1,4 +1,5 @@
 import AbortController from 'abort-controller';
+import Scheduler from 'rx-scheduler';
 import Single from '../../single';
 import { cleanObserver, isNumber } from '../utils';
 
@@ -8,9 +9,7 @@ import { cleanObserver, isNumber } from '../utils';
 function subscribeActual(observer) {
   const { onSuccess, onError, onSubscribe } = cleanObserver(observer);
 
-  const { amount, doDelayError } = this;
-
-  let timeout;
+  const { amount, scheduler, doDelayError } = this;
 
   const controller = new AbortController();
 
@@ -22,12 +21,6 @@ function subscribeActual(observer) {
     return;
   }
 
-  signal.addEventListener('abort', () => {
-    if (typeof timeout !== 'undefined') {
-      clearTimeout(timeout);
-    }
-  });
-
   this.source.subscribeWith({
     onSubscribe(ac) {
       signal.addEventListener('abort', () => {
@@ -35,29 +28,42 @@ function subscribeActual(observer) {
       });
     },
     onSuccess(x) {
-      timeout = setTimeout(() => {
+      const ac = scheduler.delay(() => {
         onSuccess(x);
         controller.abort();
       }, amount);
+
+      signal.addEventListener('abort', () => {
+        ac.abort();
+      });
     },
     onError(x) {
-      timeout = setTimeout(() => {
+      const ac = scheduler.delay(() => {
         onError(x);
         controller.abort();
       }, doDelayError ? amount : 0);
+
+      signal.addEventListener('abort', () => {
+        ac.abort();
+      });
     },
   });
 }
 /**
  * @ignore
  */
-export default (source, amount, doDelayError) => {
+export default (source, amount, scheduler, doDelayError) => {
   if (!isNumber(amount)) {
     return source;
+  }
+  let sched = scheduler;
+  if (!(sched instanceof Scheduler.interface)) {
+    sched = Scheduler.current;
   }
   const single = new Single(subscribeActual);
   single.source = source;
   single.amount = amount;
+  single.scheduler = sched;
   single.doDelayError = doDelayError;
   return single;
 };
