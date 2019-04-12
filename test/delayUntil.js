96e0d9e2c772a1742b15bd19902ddb8b0a280f21
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-12 11:18:46 +0800

    Update delayUntil.js

--- test/delayUntil.js
+++ test/delayUntil.js
@@ -5,85 +5,80 @@ import Single from '../src/single';
 /**
  *
  */
-describe('Single', () => {
+describe('#delayUntil', () => {
   /**
    *
    */
-  describe('#delayUntil', () => {
-    /**
-     *
-     */
-    it('should create a Single', () => {
-      const single = Single.just('Hello').delayUntil(Single.timer(100));
-      assert(single instanceof Single);
-    });
-    /**
-     *
-     */
-    it('should return the same instance if the provided value is not a Single.', () => {
-      const source = Single.just('Hello');
-      const single = source.delayUntil();
-      assert(source === single);
-    });
-    /**
-     *
-     */
-    it('should signal success with the given value.', (done) => {
-      const single = Single.just('Hello').delayUntil(Single.timer(100));
-      single.subscribe(
-        x => (x === 'Hello' ? done() : done(false)),
-        x => done(x),
-      );
-    });
-    /**
-     *
-     */
-    it('should signal error with the given value.', (done) => {
-      const single = Single.error(new Error('Hello')).delayUntil(Single.timer(100));
-      single.subscribe(
-        x => done(x),
-        e => (e instanceof Error ? done() : done(false)),
-      );
-    });
-    /**
-     *
-     */
-    it('should signal error if other Single signals error.', (done) => {
-      const single = Single.error(new Error('World')).delayUntil(Single.error(new Error('Hello')));
-      single.subscribe(
-        x => done(x),
-        e => (e instanceof Error ? done() : done(false)),
-      );
-    });
-    /**
-     *
-     */
-    it('should not signal success if aborted.', (done) => {
-      const source = Single.just('Hello').delayUntil(Single.timer(100));
-      const controller = source.subscribe(
-        () => done(false),
-        () => done(false),
-      );
+  it('should create a Single', () => {
+    const single = Single.just('Hello').delayUntil(Single.timer(100));
+    assert(single instanceof Single);
+  });
+  /**
+   *
+   */
+  it('should return the same instance if the provided value is not a Single.', () => {
+    const source = Single.just('Hello');
+    const single = source.delayUntil();
+    assert(source === single);
+  });
+  /**
+   *
+   */
+  it('should signal success with the given value.', (done) => {
+    const single = Single.just('Hello').delayUntil(Single.timer(100));
+    single.subscribe(
+      x => (x === 'Hello' ? done() : done(false)),
+      done,
+    );
+  });
+  /**
+   *
+   */
+  it('should signal error with the given value.', (done) => {
+    const single = Single.error(new Error('Hello')).delayUntil(Single.timer(100));
+    single.subscribe(
+      done,
+      () => done(),
+    );
+  });
+  /**
+   *
+   */
+  it('should signal error if other Single signals error.', (done) => {
+    const single = Single.error(new Error('World')).delayUntil(Single.error(new Error('Hello')));
+    single.subscribe(
+      done,
+      () => done(),
+    );
+  });
+  /**
+   *
+   */
+  it('should not signal success if cancelled.', (done) => {
+    const source = Single.just('Hello').delayUntil(Single.timer(100));
+    const controller = source.subscribe(
+      () => done(false),
+      () => done(false),
+    );
 
-      controller.abort();
-      if (controller.signal.aborted) {
-        done();
-      }
-    });
-    /**
-     *
-     */
-    it('should not signal error if aborted.', (done) => {
-      const source = Single.error(new Error('Hello')).delayUntil(Single.timer(100));
-      const controller = source.subscribe(
-        () => done(false),
-        () => done(false),
-      );
+    controller.cancel();
+    if (controller.cancelled) {
+      done();
+    }
+  });
+  /**
+   *
+   */
+  it('should not signal error if cancelled.', (done) => {
+    const source = Single.error(new Error('Hello')).delayUntil(Single.timer(100));
+    const controller = source.subscribe(
+      () => done(false),
+      () => done(false),
+    );
 
-      controller.abort();
-      if (controller.signal.aborted) {
-        done();
-      }
-    });
+    controller.cancel();
+    if (controller.cancelled) {
+      done();
+    }
   });
 });
