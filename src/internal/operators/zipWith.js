19c0568b5becddbf67ea23d4d32f46635bdd2c37
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-12 11:17:01 +0800

    Update zipWith.js

--- src/internal/operators/zipWith.js
+++ src/internal/operators/zipWith.js
@@ -1,104 +1,6 @@
-import AbortController from 'abort-controller';
 import Single from '../../single';
-import { cleanObserver, isFunction } from '../utils';
+import zip from './zip';
 
-/**
- * @ignore
- */
-const defaultZipper = (x, y) => [x, y];
-/**
- * @ignore
- */
-function subscribeActual(observer) {
-  const { onSuccess, onError, onSubscribe } = cleanObserver(observer);
-
-  let SA;
-  let SB;
-
-  const controller = new AbortController();
-
-  const { signal } = controller;
-
-  onSubscribe(controller);
-
-  if (signal.aborted) {
-    return;
-  }
-
-  const { source, other, zipper } = this;
-
-  source.subscribeWith({
-    onSubscribe(ac) {
-      signal.addEventListener('abort', () => ac.abort());
-    },
-    onSuccess(x) {
-      if (signal.aborted) {
-        return;
-      }
-      SA = x;
-
-      if (SB != null) {
-        let result;
-
-        try {
-          result = zipper(SA, SB);
-
-          if (result == null) {
-            throw new Error('Single.zipWith: zipper function returned a null value.');
-          }
-        } catch (e) {
-          onError(e);
-          controller.abort();
-          return;
-        }
-        onSuccess(result);
-        controller.abort();
-      }
-    },
-    onError(x) {
-      onError(x);
-      controller.abort();
-    },
-  });
-
-  other.subscribeWith({
-    onSubscribe(ac) {
-      if (signal.aborted) {
-        ac.abort();
-      } else {
-        signal.addEventListener('abort', () => ac.abort());
-      }
-    },
-    onSuccess(x) {
-      if (signal.aborted) {
-        return;
-      }
-      SB = x;
-
-      if (SA != null) {
-        let result;
-
-        try {
-          result = zipper(SA, SB);
-
-          if (result == null) {
-            throw new Error('Single.zipWith: zipper function returned a null value.');
-          }
-        } catch (e) {
-          onError(e);
-          controller.abort();
-          return;
-        }
-        onSuccess(result);
-        controller.abort();
-      }
-    },
-    onError(x) {
-      onError(x);
-      controller.abort();
-    },
-  });
-}
 /**
  * @ignore
  */
@@ -106,13 +8,5 @@ export default (source, other, zipper) => {
   if (!(other instanceof Single)) {
     return source;
   }
-  let fn = zipper;
-  if (!isFunction(zipper)) {
-    fn = defaultZipper;
-  }
-  const single = new Single(subscribeActual);
-  single.source = source;
-  single.other = other;
-  single.zipper = fn;
-  return single;
+  return zip([source, other], zipper);
 };
