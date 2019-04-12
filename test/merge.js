c349a3dee25742c44b32fe2f91b56b4286a7d365
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-12 11:19:28 +0800

    Update merge.js

--- test/merge.js
+++ test/merge.js
@@ -5,51 +5,46 @@ import Single from '../src/single';
 /**
  *
  */
-describe('Single', () => {
+describe('#merge', () => {
   /**
    *
    */
-  describe('#merge', () => {
-    /**
-     *
-     */
-    it('should create a Single', () => {
-      const single = Single.merge(Single.just(Single.just('Hello')));
+  it('should create a Single', () => {
+    const single = Single.merge(Single.just(Single.just('Hello')));
 
-      assert(single instanceof Single);
-    });
-    /**
-     *
-     */
-    it('should signal error if no source is provided', (done) => {
-      const single = Single.merge();
+    assert(single instanceof Single);
+  });
+  /**
+   *
+   */
+  it('should signal error if no source is provided', (done) => {
+    const single = Single.merge();
 
-      single.subscribe(
-        () => done(false),
-        () => done(),
-      );
-    });
-    /**
-     *
-     */
-    it('should signal error if source emitted a non-Single', (done) => {
-      const single = Single.merge(Single.just('Hello'));
+    single.subscribe(
+      () => done(false),
+      () => done(),
+    );
+  });
+  /**
+   *
+   */
+  it('should signal error if source emitted a non-Single', (done) => {
+    const single = Single.merge(Single.just('Hello'));
 
-      single.subscribe(
-        () => done(false),
-        () => done(),
-      );
-    });
-    /**
-     *
-     */
-    it('should signal success of the signaled Single', (done) => {
-      const single = Single.merge(Single.just(Single.just('Hello')));
+    single.subscribe(
+      () => done(false),
+      () => done(),
+    );
+  });
+  /**
+   *
+   */
+  it('should signal success of the signaled Single', (done) => {
+    const single = Single.merge(Single.just(Single.just('Hello')));
 
-      single.subscribe(
-        x => (x === 'Hello' ? done() : done(false)),
-        () => done(false),
-      );
-    });
+    single.subscribe(
+      x => (x === 'Hello' ? done() : done(false)),
+      () => done(false),
+    );
   });
 });
