3eeb3aa25f77442533014d34f524bc7bdcd2731b
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-12 11:18:25 +0800

    Update amb.js

--- test/amb.js
+++ test/amb.js
@@ -5,57 +5,52 @@ import Single from '../src/single';
 /**
  *
  */
-describe('Single', () => {
+describe('#amb', () => {
   /**
    *
    */
-  describe('#amb', () => {
-    /**
-     *
-     */
-    it('should create a Single', () => {
-      const single = Single.amb([Single.just('First'), Single.just('Second')]);
-      assert(single instanceof Single);
-    });
-    /**
-     *
-     */
-    it('should signal success from the earliest source.', (done) => {
-      const single = Single.amb([Single.just('Hello'), Single.timer(100)]);
-      single.subscribe(
-        () => done(),
-        done,
-      );
-    });
-    /**
-     *
-     */
-    it('should signal error from the earliest source.', (done) => {
-      const single = Single.amb([Single.error(new Error('Hello')), Single.timer(100)]);
-      single.subscribe(
-        done,
-        () => done(),
-      );
-    });
-    /**
-     *
-     */
-    it('should signal error if one of the source is non-Single.', (done) => {
-      const single = Single.amb(['Hello', Single.timer(100)]);
-      single.subscribe(
-        done,
-        e => (e instanceof Error ? done() : done(e)),
-      );
-    });
-    /**
-     *
-     */
-    it('should signal error if given argument is not Iterable', (done) => {
-      const single = Single.amb();
-      single.subscribe(
-        done,
-        e => (e instanceof Error ? done() : done(e)),
-      );
-    });
+  it('should create a Single', () => {
+    const single = Single.amb([Single.just('First'), Single.just('Second')]);
+    assert(single instanceof Single);
+  });
+  /**
+   *
+   */
+  it('should signal success from the earliest source.', (done) => {
+    const single = Single.amb([Single.just('Hello'), Single.timer(100)]);
+    single.subscribe(
+      () => done(),
+      done,
+    );
+  });
+  /**
+   *
+   */
+  it('should signal error from the earliest source.', (done) => {
+    const single = Single.amb([Single.error(new Error('Hello')), Single.timer(100)]);
+    single.subscribe(
+      done,
+      () => done(),
+    );
+  });
+  /**
+   *
+   */
+  it('should signal error if one of the source is non-Single.', (done) => {
+    const single = Single.amb(['Hello', Single.timer(100)]);
+    single.subscribe(
+      done,
+      e => (e instanceof Error ? done() : done(e)),
+    );
+  });
+  /**
+   *
+   */
+  it('should signal error if given argument is not Iterable', (done) => {
+    const single = Single.amb();
+    single.subscribe(
+      done,
+      e => (e instanceof Error ? done() : done(e)),
+    );
   });
 });
