d8f86ba0090f9c04f58f6fc6eed32da6a32e1e78
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-12 11:18:34 +0800

    Update compose.js

--- test/compose.js
+++ test/compose.js
@@ -5,49 +5,44 @@ import Single from '../src/single';
 /**
  *
  */
-describe('Single', () => {
+describe('#compose', () => {
   /**
    *
    */
-  describe('#compose', () => {
-    /**
-     *
-     */
-    it('should return the same instance if no function is provided', () => {
-      const source = Single.just('Hello');
-      const single = source.compose();
+  it('should return the same instance if no function is provided', () => {
+    const source = Single.just('Hello');
+    const single = source.compose();
 
-      assert(source === single);
-    });
-    /**
-     *
-     */
-    it('should return a Single from the transformer', () => {
-      const single = Single.just('Hello').compose(source => source.map(x => `${x} World`));
+    assert(source === single);
+  });
+  /**
+   *
+   */
+  it('should return a Single from the transformer', () => {
+    const single = Single.just('Hello').compose(source => source.map(x => `${x} World`));
 
-      assert(single instanceof Single);
-    });
-    /**
-     *
-     */
-    it('should correctly signal the composed Single', (done) => {
-      const single = Single.just('Hello').compose(source => source.map(x => `${x} World`));
+    assert(single instanceof Single);
+  });
+  /**
+   *
+   */
+  it('should correctly signal the composed Single', (done) => {
+    const single = Single.just('Hello').compose(source => source.map(x => `${x} World`));
 
-      single.subscribe(
-        x => (x === 'Hello World' ? done() : done(x)),
-        done,
-      );
-    });
-    /**
-     *
-     */
-    it('should signal error if the transformer function returned a non-Single', (done) => {
-      const single = Single.just('Hello').compose(() => undefined);
+    single.subscribe(
+      x => (x === 'Hello World' ? done() : done(x)),
+      done,
+    );
+  });
+  /**
+   *
+   */
+  it('should signal error if the transformer function returned a non-Single', (done) => {
+    const single = Single.just('Hello').compose(() => undefined);
 
-      single.subscribe(
-        done,
-        () => done(),
-      );
-    });
+    single.subscribe(
+      done,
+      () => done(),
+    );
   });
 });
