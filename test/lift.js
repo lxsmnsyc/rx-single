ce4412a5306242f6249143e98d9c492b4d957439
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-12 11:19:25 +0800

    Update lift.js

--- test/lift.js
+++ test/lift.js
@@ -1,43 +1,39 @@
 /* eslint-disable no-undef */
 import assert from 'assert';
 import Single from '../src/single';
+
 /**
  *
  */
-describe('Single', () => {
+describe('#lift', () => {
   /**
    *
    */
-  describe('#lift', () => {
-    /**
-     *
-     */
-    it('should return the same instance if no function is provided', () => {
-      const source = Single.just('Hello');
-      const single = source.lift();
+  it('should return the same instance if no function is provided', () => {
+    const source = Single.just('Hello');
+    const single = source.lift();
 
-      assert(source === single);
-    });
-    /**
-     *
-     */
-    it('should signal an error if the lift operator returned a non-Observer', (done) => {
-      const single = Single.just('Hello').lift(() => {});
+    assert(source === single);
+  });
+  /**
+   *
+   */
+  it('should signal an error if the lift operator returned a non-Observer', (done) => {
+    const single = Single.just('Hello').lift(() => {});
 
-      single.subscribe(
-        () => done(false),
-        () => done(),
-      );
-    });
-    /**
-     *
-     */
-    it('should subscribe successfully', (done) => {
-      const single = Single.just('Hello').lift(observer => ({ onSubscribe: observer.onSubscribe, onSuccess: observer.onSuccess }));
+    single.subscribe(
+      () => done(false),
+      () => done(),
+    );
+  });
+  /**
+   *
+   */
+  it('should subscribe successfully', (done) => {
+    const single = Single.just('Hello').lift(observer => ({ onSubscribe: observer.onSubscribe, onSuccess: observer.onSuccess }));
 
-      single.subscribe(
-        () => done(),
-      );
-    });
+    single.subscribe(
+      () => done(),
+    );
   });
 });
