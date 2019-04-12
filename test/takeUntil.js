04cf11d094ebadeb9bfb5fe7e696e7d5fee82fd7
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-12 11:19:39 +0800

    Update takeUntil.js

--- test/takeUntil.js
+++ test/takeUntil.js
@@ -5,82 +5,77 @@ import Single from '../src/single';
 /**
  *
  */
-describe('Single', () => {
+describe('#takeUntil', () => {
   /**
    *
    */
-  describe('#takeUntil', () => {
-    /**
-     *
-     */
-    it('should create a Single', () => {
-      const single = Single.just('Hello').takeUntil(Single.timer(100));
+  it('should create a Single', () => {
+    const single = Single.just('Hello').takeUntil(Single.timer(100));
 
-      assert(single instanceof Single);
-    });
-    /**
-     *
-     */
-    it('should return the same instance if no other Single is provided', () => {
-      const source = Single.just('Hello');
-      const single = source.takeUntil();
+    assert(single instanceof Single);
+  });
+  /**
+   *
+   */
+  it('should return the same instance if no other Single is provided', () => {
+    const source = Single.just('Hello');
+    const single = source.takeUntil();
 
-      assert(source === single);
-    });
-    /**
-     *
-     */
-    it('should signal success if other Single has not emitted a success signal', (done) => {
-      const single = Single.just('Hello').takeUntil(Single.timer(100));
+    assert(source === single);
+  });
+  /**
+   *
+   */
+  it('should signal success if other Single has not emitted a success signal', (done) => {
+    const single = Single.just('Hello').takeUntil(Single.timer(100));
 
-      single.subscribe(
-        x => (x === 'Hello' ? done() : done(false)),
-        done,
-      );
-    });
-    /**
-     *
-     */
-    it('should signal success if other Single has not emitted an error signal', (done) => {
-      const single = Single.just('Hello').takeUntil(Single.error(new Error('World')).delay(100));
+    single.subscribe(
+      x => (x === 'Hello' ? done() : done(false)),
+      done,
+    );
+  });
+  /**
+   *
+   */
+  it('should signal success if other Single has not emitted an error signal', (done) => {
+    const single = Single.just('Hello').takeUntil(Single.error(new Error('World')).delay(100));
 
-      single.subscribe(
-        x => (x === 'Hello' ? done() : done(false)),
-        done,
-      );
-    });
-    /**
-     *
-     */
-    it('should signal error if other Single has emitted a success signal', (done) => {
-      const single = Single.just('Hello').delay(100).takeUntil(Single.just('World'));
+    single.subscribe(
+      x => (x === 'Hello' ? done() : done(false)),
+      done,
+    );
+  });
+  /**
+   *
+   */
+  it('should signal error if other Single has emitted a success signal', (done) => {
+    const single = Single.just('Hello').delay(100).takeUntil(Single.just('World'));
 
-      single.subscribe(
-        done,
-        () => done(),
-      );
-    });
-    /**
-     *
-     */
-    it('should signal error if other Single has emitted an error signal', (done) => {
-      const single = Single.just('Hello').delay(100).takeUntil(Single.error(new Error('World')));
+    single.subscribe(
+      done,
+      () => done(),
+    );
+  });
+  /**
+   *
+   */
+  it('should signal error if other Single has emitted an error signal', (done) => {
+    const single = Single.just('Hello').delay(100).takeUntil(Single.error(new Error('World')));
 
-      single.subscribe(
-        done,
-        () => done(),
-      );
-    });
-    /**
-     *
-     */
-    it('should signal error if source signals error, nonetheless', (done) => {
-      const single = Single.error(new Error('Hello')).takeUntil(Single.timer(100));
+    single.subscribe(
+      done,
+      () => done(),
+    );
+  });
+  /**
+   *
+   */
+  it('should signal error if source signals error, nonetheless', (done) => {
+    const single = Single.error(new Error('Hello')).takeUntil(Single.timer(100));
 
-      single.subscribe(
-        done,
-        () => done(),
-      );
-    });
+    single.subscribe(
+      done,
+      () => done(),
+    );
   });
 });
