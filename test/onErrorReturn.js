92ba1d3fc37585cbda26e832124d2085cf4bf2db
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-12 11:19:34 +0800

    Update onErrorReturn.js

--- test/onErrorReturn.js
+++ test/onErrorReturn.js
@@ -5,56 +5,51 @@ import Single from '../src/single';
 /**
  *
  */
-describe('Single', () => {
+describe('#onErrorReturn', () => {
   /**
    *
    */
-  describe('#onErrorReturn', () => {
-    /**
-     *
-     */
-    it('should create a Single', () => {
-      const single = Single.error(new Error('Hello')).onErrorReturn(() => 'World');
-      assert(single instanceof Single);
-    });
-    /**
-     *
-     */
-    it('should return the same instance if parameter passed is not a Single or a function', () => {
-      const source = Single.error(new Error('Hello'));
-      const single = source.onErrorReturn();
-      assert(single === source);
-    });
-    /**
-     *
-     */
-    it('should emit the supplied item by the given function in case of error', (done) => {
-      const single = Single.error(new Error('Hello')).onErrorReturn(() => 'World');
+  it('should create a Single', () => {
+    const single = Single.error(new Error('Hello')).onErrorReturn(() => 'World');
+    assert(single instanceof Single);
+  });
+  /**
+   *
+   */
+  it('should return the same instance if parameter passed is not a Single or a function', () => {
+    const source = Single.error(new Error('Hello'));
+    const single = source.onErrorReturn();
+    assert(single === source);
+  });
+  /**
+   *
+   */
+  it('should emit the supplied item by the given function in case of error', (done) => {
+    const single = Single.error(new Error('Hello')).onErrorReturn(() => 'World');
 
-      single.subscribe(
-        () => done(),
-        done,
-      );
-    });
-    /**
-     *
-     */
-    it('should emit error if provide function throws error.', (done) => {
-      const single = Single.error(new Error('Hello')).onErrorReturn(() => { throw new Error('Ooops'); });
-      single.subscribe(
-        done,
-        () => done(),
-      );
-    });
-    /**
-     *
-     */
-    it('should emit error if provide function returns undefined.', (done) => {
-      const single = Single.error(new Error('Hello')).onErrorReturn(() => {});
-      single.subscribe(
-        done,
-        () => done(),
-      );
-    });
+    single.subscribe(
+      () => done(),
+      done,
+    );
+  });
+  /**
+   *
+   */
+  it('should emit error if provide function throws error.', (done) => {
+    const single = Single.error(new Error('Hello')).onErrorReturn(() => { throw new Error('Ooops'); });
+    single.subscribe(
+      done,
+      () => done(),
+    );
+  });
+  /**
+   *
+   */
+  it('should emit error if provide function returns undefined.', (done) => {
+    const single = Single.error(new Error('Hello')).onErrorReturn(() => {});
+    single.subscribe(
+      done,
+      () => done(),
+    );
   });
 });
