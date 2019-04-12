e8d68c68b4ac3a741dd7faeff40630c5c29a2243
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-12 11:18:59 +0800

    Update doOnEvent.js

--- test/doOnEvent.js
+++ test/doOnEvent.js
@@ -5,49 +5,44 @@ import Single from '../src/single';
 /**
  *
  */
-describe('Single', () => {
+describe('#doOnEvent', () => {
   /**
    *
    */
-  describe('#doOnEvent', () => {
-    /**
-     *
-     */
-    it('should create a Single', () => {
-      const single = Single.just('Hello').doOnEvent(() => {});
-      assert(single instanceof Single);
-    });
-    /**
-     *
-     */
-    it('should return the same instance if the method received a non-function parameter.', () => {
-      const source = Single.just('Hello');
-      const single = source.doOnEvent();
-      assert(source === single);
-    });
-    /**
-     *
-     */
-    it('should call the given function on success.', (done) => {
-      let called;
-      const source = Single.just('Hello');
-      const single = source.doOnEvent(() => { called = true; });
-      single.subscribe(
-        () => called && done(),
-        () => done(false),
-      );
-    });
-    /**
-     *
-     */
-    it('should call the given function on error.', (done) => {
-      let called;
-      const source = Single.error(new Error('Hello'));
-      const single = source.doOnEvent(() => { called = true; });
-      single.subscribe(
-        () => done(false),
-        () => called && done(),
-      );
-    });
+  it('should create a Single', () => {
+    const single = Single.just('Hello').doOnEvent(() => {});
+    assert(single instanceof Single);
+  });
+  /**
+   *
+   */
+  it('should return the same instance if the method received a non-function parameter.', () => {
+    const source = Single.just('Hello');
+    const single = source.doOnEvent();
+    assert(source === single);
+  });
+  /**
+   *
+   */
+  it('should call the given function on success.', (done) => {
+    let called;
+    const source = Single.just('Hello');
+    const single = source.doOnEvent(() => { called = true; });
+    single.subscribe(
+      () => called && done(),
+      () => done(false),
+    );
+  });
+  /**
+   *
+   */
+  it('should call the given function on error.', (done) => {
+    let called;
+    const source = Single.error(new Error('Hello'));
+    const single = source.doOnEvent(() => { called = true; });
+    single.subscribe(
+      () => done(false),
+      () => called && done(),
+    );
   });
 });
