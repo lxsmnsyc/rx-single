e7c4cec1f14f19e5f0b00c9cf1cc2719f91ca659
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-12 11:19:36 +0800

    Update onErrorReturnItem.js

--- test/onErrorReturnItem.js
+++ test/onErrorReturnItem.js
@@ -5,36 +5,31 @@ import Single from '../src/single';
 /**
  *
  */
-describe('Single', () => {
+describe('#onErrorReturnItem', () => {
   /**
    *
    */
-  describe('#onErrorReturnItem', () => {
-    /**
-     *
-     */
-    it('should create a Single', () => {
-      const single = Single.error(new Error('Hello')).onErrorReturnItem('World');
-      assert(single instanceof Single);
-    });
-    /**
-     *
-     */
-    it('should return the same instance if parameter passed is not a Single or a function', () => {
-      const source = Single.error(new Error('Hello'));
-      const single = source.onErrorReturnItem();
-      assert(single === source);
-    });
-    /**
-     *
-     */
-    it('should emit the given item in case of error', (done) => {
-      const single = Single.error(new Error('Hello')).onErrorReturnItem('World');
+  it('should create a Single', () => {
+    const single = Single.error(new Error('Hello')).onErrorReturnItem('World');
+    assert(single instanceof Single);
+  });
+  /**
+   *
+   */
+  it('should return the same instance if parameter passed is not a Single or a function', () => {
+    const source = Single.error(new Error('Hello'));
+    const single = source.onErrorReturnItem();
+    assert(single === source);
+  });
+  /**
+   *
+   */
+  it('should emit the given item in case of error', (done) => {
+    const single = Single.error(new Error('Hello')).onErrorReturnItem('World');
 
-      single.subscribe(
-        x => (x === 'World' ? done() : done(false)),
-        done,
-      );
-    });
+    single.subscribe(
+      x => (x === 'World' ? done() : done(false)),
+      done,
+    );
   });
 });
