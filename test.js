79045490b17cc5bb52cccd7bbcbb1396ea21392a
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-12 11:23:52 +0800

    Delete test.js

--- test.js
+++
@@ -1,5 +0,0 @@
-const Single = require('./index');
-
-const controller = Single.create((e) => {
-  e.onSuccess('Hello World');
-}).doOnCancel(() => console.log('Cancelled')).delaySubscription(1000).subscribe(console.log, console.log);
