07e952360a43e2f760b4e5e6b26357d005a11d35
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-12 11:19:27 +0800

    Update map.js

--- test/map.js
+++ test/map.js
@@ -5,63 +5,58 @@ import Single from '../src/single';
 /**
  *
  */
-describe('Single', () => {
+describe('#map', () => {
   /**
    *
    */
-  describe('#map', () => {
-    /**
-     *
-     */
-    it('should create a Single', () => {
-      const single = Single.just('Hello').map(x => `${x} World`);
-      assert(single instanceof Single);
-    });
-    /**
-     *
-     */
-    it('should succeed with the given value.', (done) => {
-      const single = Single.just('Hello').map(x => `${x} World`);
+  it('should create a Single', () => {
+    const single = Single.just('Hello').map(x => `${x} World`);
+    assert(single instanceof Single);
+  });
+  /**
+   *
+   */
+  it('should succeed with the given value.', (done) => {
+    const single = Single.just('Hello').map(x => `${x} World`);
 
-      single.subscribe(
-        x => (x === 'Hello World' ? done() : done(false)),
-        e => done(e),
-      );
+    single.subscribe(
+      x => (x === 'Hello World' ? done() : done(false)),
+      done,
+    );
+  });
+  /**
+   *
+   */
+  it('should signal an error if the mapper throws an error', (done) => {
+    const single = Single.just('Hello').map(() => {
+      throw new Error('Expected');
     });
-    /**
-     *
-     */
-    it('should signal an error if the mapper throws an error', (done) => {
-      const single = Single.just('Hello').map(() => {
-        throw new Error('Expected');
-      });
 
-      single.subscribe(
-        () => done(false),
-        e => (e instanceof Error ? done() : done(false)),
-      );
-    });
-    /**
-     *
-     */
-    it('should signal an error if the mapper throws an error', (done) => {
-      const single = Single.just('Hello').map(() => undefined);
+    single.subscribe(
+      () => done(false),
+      e => (e instanceof Error ? done() : done(false)),
+    );
+  });
+  /**
+   *
+   */
+  it('should signal an error if the mapper throws an error', (done) => {
+    const single = Single.just('Hello').map(() => undefined);
 
-      single.subscribe(
-        () => done(false),
-        e => (e instanceof Error ? done() : done(false)),
-      );
-    });
-    /**
-     *
-     */
-    it('should retain the value if no function is supplied.', (done) => {
-      const single = Single.just('Hello').map();
+    single.subscribe(
+      () => done(false),
+      e => (e instanceof Error ? done() : done(false)),
+    );
+  });
+  /**
+   *
+   */
+  it('should retain the value if no function is supplied.', (done) => {
+    const single = Single.just('Hello').map();
 
-      single.subscribe(
-        x => (x === 'Hello' ? done() : done(false)),
-        e => done(e),
-      );
-    });
+    single.subscribe(
+      x => (x === 'Hello' ? done() : done(false)),
+      done,
+    );
   });
 });
