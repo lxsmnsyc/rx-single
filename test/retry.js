97157ed4f68d6ed0de4f9444f3099a6e8104d469
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-12 11:19:37 +0800

    Update retry.js

--- test/retry.js
+++ test/retry.js
@@ -5,57 +5,52 @@ import Single from '../src/single';
 /**
  *
  */
-describe('Single', () => {
+describe('#retry', () => {
   /**
    *
    */
-  describe('#retry', () => {
-    /**
-     *
-     */
-    it('should create a Single', () => {
-      const single = Single.error(new Error('Hello')).retry(x => x === 3);
+  it('should create a Single', () => {
+    const single = Single.error(new Error('Hello')).retry(x => x === 3);
 
-      assert(single instanceof Single);
-    });
-    /**
-     *
-     */
-    it('should signal a success if no error', (done) => {
-      const single = Single.just('Hello').retry(x => x === 3);
+    assert(single instanceof Single);
+  });
+  /**
+   *
+   */
+  it('should signal a success if no error', (done) => {
+    const single = Single.just('Hello').retry(x => x === 3);
 
-      single.subscribe(
-        () => done(),
-        () => done(false),
-      );
+    single.subscribe(
+      () => done(),
+      () => done(false),
+    );
+  });
+  /**
+   *
+   */
+  it('should retry if there is an error and if it passes the predicate', (done) => {
+    let retried;
+    const single = Single.error(new Error('Hello')).retry((x) => {
+      if (x === 2) {
+        retried = true;
+      }
+      return x < 3;
     });
-    /**
-     *
-     */
-    it('should retry if there is an error and if it passes the predicate', (done) => {
-      let retried;
-      const single = Single.error(new Error('Hello')).retry((x) => {
-        if (x === 2) {
-          retried = true;
-        }
-        return x < 3;
-      });
 
-      single.subscribe(
-        () => done(false),
-        () => retried && done(),
-      );
-    });
-    /**
-     *
-     */
-    it('should signal an error if predicate is false', (done) => {
-      const single = Single.error(new Error('Hello')).retry(x => x === 3);
+    single.subscribe(
+      () => done(false),
+      () => retried && done(),
+    );
+  });
+  /**
+   *
+   */
+  it('should signal an error if predicate is false', (done) => {
+    const single = Single.error(new Error('Hello')).retry(x => x === 3);
 
-      single.subscribe(
-        () => done(false),
-        () => done(),
-      );
-    });
+    single.subscribe(
+      () => done(false),
+      () => done(),
+    );
   });
 });
