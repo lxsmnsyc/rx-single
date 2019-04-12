590807a4a4b4079b762cb3b08f41af3be1c9c7e8
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-12 11:19:32 +0800

    Update onErrorResumeNext.js

--- test/onErrorResumeNext.js
+++ test/onErrorResumeNext.js
@@ -5,65 +5,60 @@ import Single from '../src/single';
 /**
  *
  */
-describe('Single', () => {
+describe('#onErrorResumeNext', () => {
   /**
    *
    */
-  describe('#onErrorResumeNext', () => {
-    /**
-     *
-     */
-    it('should create a Single', () => {
-      const single = Single.error(new Error('Hello')).onErrorResumeNext(Single.just('World'));
-      assert(single instanceof Single);
-    });
-    /**
-     *
-     */
-    it('should return the same instance if parameter passed is not a Single or a function', () => {
-      const source = Single.error(new Error('Hello'));
-      const single = source.onErrorResumeNext();
-      assert(single === source);
-    });
-    /**
-     *
-     */
-    it('should subscribe to the given Single', (done) => {
-      const single = Single.error(new Error('Hello')).onErrorResumeNext(Single.just('World'));
-      single.subscribe(
-        () => done(),
-        done,
-      );
-    });
-    /**
-     *
-     */
-    it('should subscribe to the given Single-producing Function', (done) => {
-      const single = Single.error(new Error('Hello')).onErrorResumeNext(() => Single.just('World'));
-      single.subscribe(
-        () => done(),
-        done,
-      );
-    });
-    /**
-     *
-     */
-    it('should emit error if provide function throws error.', (done) => {
-      const single = Single.error(new Error('Hello')).onErrorResumeNext(() => { throw new Error('Ooops'); });
-      single.subscribe(
-        done,
-        () => done(),
-      );
-    });
-    /**
-     *
-     */
-    it('should emit error if provide function returns non-Single.', (done) => {
-      const single = Single.error(new Error('Hello')).onErrorResumeNext(() => {});
-      single.subscribe(
-        done,
-        () => done(),
-      );
-    });
+  it('should create a Single', () => {
+    const single = Single.error(new Error('Hello')).onErrorResumeNext(Single.just('World'));
+    assert(single instanceof Single);
+  });
+  /**
+   *
+   */
+  it('should return the same instance if parameter passed is not a Single or a function', () => {
+    const source = Single.error(new Error('Hello'));
+    const single = source.onErrorResumeNext();
+    assert(single === source);
+  });
+  /**
+   *
+   */
+  it('should subscribe to the given Single', (done) => {
+    const single = Single.error(new Error('Hello')).onErrorResumeNext(Single.just('World'));
+    single.subscribe(
+      () => done(),
+      done,
+    );
+  });
+  /**
+   *
+   */
+  it('should subscribe to the given Single-producing Function', (done) => {
+    const single = Single.error(new Error('Hello')).onErrorResumeNext(() => Single.just('World'));
+    single.subscribe(
+      () => done(),
+      done,
+    );
+  });
+  /**
+   *
+   */
+  it('should emit error if provide function throws error.', (done) => {
+    const single = Single.error(new Error('Hello')).onErrorResumeNext(() => { throw new Error('Ooops'); });
+    single.subscribe(
+      done,
+      () => done(),
+    );
+  });
+  /**
+   *
+   */
+  it('should emit error if provide function returns non-Single.', (done) => {
+    const single = Single.error(new Error('Hello')).onErrorResumeNext(() => {});
+    single.subscribe(
+      done,
+      () => done(),
+    );
   });
 });
