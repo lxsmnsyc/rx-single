731df69c90688e53f55cb84c7e8afe139fea61f0
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-12 11:19:01 +0800

    Update doOnSubscribe.js

--- test/doOnSubscribe.js
+++ test/doOnSubscribe.js
@@ -5,41 +5,36 @@ import Single from '../src/single';
 /**
  *
  */
-describe('Single', () => {
+describe('#doOnSubscribe', () => {
   /**
    *
    */
-  describe('#doOnSubscribe', () => {
-    /**
-     *
-     */
-    it('should create a Single', () => {
-      const single = Single.just('Hello').doOnSubscribe(() => {});
-      assert(single instanceof Single);
-    });
-    /**
-     *
-     */
-    it('should return the same instance if no function is passed', () => {
-      const source = Single.just('Hello');
-      const single = source.doOnSubscribe();
-      assert(source === single);
-    });
-    /**
-     *
-     */
-    it('should be called before actual subscription.', (done) => {
-      let called;
-      const single = Single.just('Hello').doOnSubscribe(() => { called = true; });
-      single.subscribeWith({
-        onSubscribe() {
-          if (called) {
-            done();
-          } else {
-            done(false);
-          }
-        },
-      });
+  it('should create a Single', () => {
+    const single = Single.just('Hello').doOnSubscribe(() => {});
+    assert(single instanceof Single);
+  });
+  /**
+   *
+   */
+  it('should return the same instance if no function is passed', () => {
+    const source = Single.just('Hello');
+    const single = source.doOnSubscribe();
+    assert(source === single);
+  });
+  /**
+   *
+   */
+  it('should be called before actual subscription.', (done) => {
+    let called;
+    const single = Single.just('Hello').doOnSubscribe(() => { called = true; });
+    single.subscribeWith({
+      onSubscribe() {
+        if (called) {
+          done();
+        } else {
+          done(false);
+        }
+      },
     });
   });
 });
