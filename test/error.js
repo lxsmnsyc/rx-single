3250ee3b8ac3f0fc898a4acb684466e1180c4409
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-12 11:19:07 +0800

    Update error.js

--- test/error.js
+++ test/error.js
@@ -5,64 +5,59 @@ import Single from '../src/single';
 /**
  *
  */
-describe('Single', () => {
+describe('#error', () => {
   /**
    *
    */
-  describe('#error', () => {
-    /**
-     *
-     */
-    it('should create a Single', () => {
-      const single = Single.error(new Error('Hello World'));
+  it('should create a Single', () => {
+    const single = Single.error(new Error('Hello World'));
 
-      assert(single instanceof Single);
-    });
-    /**
-     *
-     */
-    it('should error with the given value.', (done) => {
-      const single = Single.error(new Error('Hello World'));
+    assert(single instanceof Single);
+  });
+  /**
+   *
+   */
+  it('should error with the given value.', (done) => {
+    const single = Single.error(new Error('Hello World'));
 
-      single.subscribe(
-        () => done(false),
-        () => done(),
-      );
-    });
-    /**
-     *
-     */
-    it('should error with the a message if the given value is undefined', (done) => {
-      const single = Single.error();
+    single.subscribe(
+      () => done(false),
+      () => done(),
+    );
+  });
+  /**
+   *
+   */
+  it('should error with the a message if the given value is undefined', (done) => {
+    const single = Single.error();
 
-      single.subscribe(
-        () => done(false),
-        () => done(),
-      );
-    });
-    /**
-     *
-     */
-    it('should error with the a message if the callable returned undefined', (done) => {
-      const single = Single.error(() => {});
+    single.subscribe(
+      () => done(false),
+      () => done(),
+    );
+  });
+  /**
+   *
+   */
+  it('should error with the a message if the callable returned undefined', (done) => {
+    const single = Single.error(() => {});
 
-      single.subscribe(
-        () => done(false),
-        () => done(),
-      );
+    single.subscribe(
+      () => done(false),
+      () => done(),
+    );
+  });
+  /**
+   *
+   */
+  it('should error if the callable throws an error.', (done) => {
+    const single = Single.error(() => {
+      throw new Error('Expected');
     });
-    /**
-     *
-     */
-    it('should error if the callable throws an error.', (done) => {
-      const single = Single.error(() => {
-        throw new Error('Expected');
-      });
 
-      single.subscribe(
-        () => done(false),
-        () => done(),
-      );
-    });
+    single.subscribe(
+      () => done(false),
+      () => done(),
+    );
   });
 });
