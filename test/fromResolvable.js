263e1580c82dd482143871b9d65b16538dedb265
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-12 11:19:16 +0800

    Update fromResolvable.js

--- test/fromResolvable.js
+++ test/fromResolvable.js
@@ -5,39 +5,45 @@ import Single from '../src/single';
 /**
  *
  */
-describe('Single', () => {
+describe('#fromResolvable', () => {
   /**
    *
    */
-  describe('#fromResolvable', () => {
-    /**
-     *
-     */
-    it('should create a Single', () => {
-      const single = Single.fromResolvable(res => res('Hello World'));
-      assert(single instanceof Single);
-    });
-    /**
-     *
-     */
-    it('should succeed with the given value.', (done) => {
-      const single = Single.fromResolvable(res => res('Hello World'));
+  it('should create a Single', () => {
+    const single = Single.fromResolvable(res => res('Hello World'));
+    assert(single instanceof Single);
+  });
+  /**
+   *
+   */
+  it('should succeed with the given value.', (done) => {
+    const single = Single.fromResolvable(res => res('Hello World'));
 
-      single.subscribe(
-        x => (x === 'Hello World' ? done() : done(false)),
-        e => done(e),
-      );
-    });
-    /**
-     *
-     */
-    it('should signal error if the given value is not a function', (done) => {
-      const single = Single.fromResolvable();
+    single.subscribe(
+      x => (x === 'Hello World' ? done() : done(false)),
+      done,
+    );
+  });
+  /**
+   *
+   */
+  it('should error with the given error.', (done) => {
+    const single = Single.fromResolvable((res, rej) => rej(new Error('Hello World')));
 
-      single.subscribe(
-        () => done(false),
-        () => done(),
-      );
-    });
+    single.subscribe(
+      done,
+      () => done(),
+    );
+  });
+  /**
+   *
+   */
+  it('should signal error if the given value is not a function', (done) => {
+    const single = Single.fromResolvable();
+
+    single.subscribe(
+      () => done(false),
+      () => done(),
+    );
   });
 });
