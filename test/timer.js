21b659112049c67d2ae64e4770a40f2ac267db3f
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-12 11:19:44 +0800

    Update timer.js

--- test/timer.js
+++ test/timer.js
@@ -5,54 +5,49 @@ import Single from '../src/single';
 /**
  *
  */
-describe('Single', () => {
+describe('#timer', () => {
   /**
    *
    */
-  describe('#timer', () => {
-    /**
-     *
-     */
-    it('should create a Single', () => {
-      const single = Single.timer(100);
-      assert(single instanceof Single);
-    });
-    /**
-     *
-     */
-    it('should signal error if amount is not a number.', (done) => {
-      const single = Single.timer();
+  it('should create a Single', () => {
+    const single = Single.timer(100);
+    assert(single instanceof Single);
+  });
+  /**
+   *
+   */
+  it('should signal error if amount is not a number.', (done) => {
+    const single = Single.timer();
 
-      single.subscribe(
-        () => done(false),
-        () => done(),
-      );
-    });
-    /**
-     *
-     */
-    it('should signal success 0', (done) => {
-      const single = Single.timer(100);
-      single.subscribe(
-        x => (x === 0 ? done() : done(false)),
-        () => done(false),
-      );
-    });
-    /**
-     *
-     */
-    it('should not signal success if aborted.', (done) => {
-      const single = Single.timer(100);
-      const controller = single.subscribe(
-        () => done(false),
-        () => done(false),
-      );
+    single.subscribe(
+      () => done(false),
+      () => done(),
+    );
+  });
+  /**
+   *
+   */
+  it('should signal success 0', (done) => {
+    const single = Single.timer(100);
+    single.subscribe(
+      x => (x === 0 ? done() : done(false)),
+      () => done(false),
+    );
+  });
+  /**
+   *
+   */
+  it('should not signal success if cancelled.', (done) => {
+    const single = Single.timer(100);
+    const controller = single.subscribe(
+      () => done(false),
+      () => done(false),
+    );
 
 
-      controller.abort();
-      if (controller.signal.aborted) {
-        done();
-      }
-    });
+    controller.cancel();
+    if (controller.cancelled) {
+      done();
+    }
   });
 });
