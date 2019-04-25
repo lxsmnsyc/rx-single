c1ccfdfe00f0f49ffd24a6e87778b3448e07b648
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-25 18:25:29 +0800

    Create zip.js

---
+++ src/internal/operators/zip.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-restricted-syntax */
+import { isIterable } from '../utils';
+import error from './error';
+import is from '../is';
+import zipArray from './zipArray';
+/**
+ * @ignore
+ */
+export default (sources, zipper) => {
+  if (!isIterable(sources)) {
+    return error(new Error('Single.zip: sources is a non-Iterable.'));
+  }
+
+  const singles = [];
+
+  for (const source of sources) {
+    if (is(source)) {
+      singles.push(sources);
+    } else {
+      return error(new Error('Observable.zip: one of the sources is not a Single.'));
+    }
+  }
+  return zipArray(singles, zipper);
+};
