e155f2232e2d59197c4464518b8139a109548a7a
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-26 08:52:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -263,12 +263,11 @@ var ambArray = (sources) => {
 /**
  * @ignore
  */
-var ambWith = (source, other) => {
-  if (!(is(other))) {
-    return source;
-  }
-  return ambArray([source, other]);
-};
+var ambWith = (source, other) => (
+  !is(other)
+    ? source
+    : ambArray([source, other])
+);
 
 /**
  * @ignore
@@ -1798,12 +1797,11 @@ var zip = (sources, zipper) => {
 /**
  * @ignore
  */
-var zipWith = (source, other, zipper) => {
-  if (!is(other)) {
-    return source;
-  }
-  return zipArray([source, other], zipper);
-};
+var zipWith = (source, other, zipper) => (
+  !is(other)
+    ? source
+    : zipArray([source, other], zipper)
+);
 
 /* eslint-disable import/no-cycle */
 
